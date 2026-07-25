8cc63242d816822b20ba5c04c1004b6b7be2158a
AbdulMuqeet Mohammed <abdulmuqeet.mohammed@yahoo.com>
2026-07-25 08:42:41 -0400

    Drizzle Fixing

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -4,7 +4,7 @@ export default {
   schema: './src/lib/server/db/schema.ts',
   out: './drizzle',
   dialect: 'sqlite',
-  driver: 'd1',
+  driver: 'd1-http',
   dbCredentials: {
     wranglerConfigPath: './wrangler.toml',
     dbName: 'masjid-db',
